e6654c6da0aed3702658261792bd3a7df5eed0dd
cegijon <53779413+cegijon@users.noreply.github.com>
2020-04-02 18:47:34 +1100

    seguimos practicando estados en React

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const [luzPrendida, setluzPrendida] = useState(false)
+
+  const handleClick = () => {
+    setluzPrendida(!luzPrendida)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`${luzPrendida ? "luzPrendida" : "luzApagada"} container`}>
+      <button onClick={handleClick}>
+        {luzPrendida ? "Apagar la luz" : "Prender la luz"}</button>
     </div>
-  );
-}
+  )
+};
 
 export default App;
